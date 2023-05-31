62e62da52f0fc528b3b45aa1b338725a0d472754
randl <spinthemers23@gmail.com>
2023-05-31 11:43:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace Homework;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        //Завдання 3
+
+        Console.WriteLine("Введіть число:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(Cirakuz(number));
+
+        int Cirakuz(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return Cirakuz(number / 2);
+            }
+            else if(number % 2 == 1 && number > 1)
+            {
+                return Cirakuz(number * 3 + 1);
+            }
+            return number; //цей return вже повертає одиницю
+        }
+    }
+}
